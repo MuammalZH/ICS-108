45fd628cf2d3372ce06323eebbe537ea56f4e77f
MuammalZH <120335187+MuammalZH@users.noreply.github.com>
2023-01-25 13:17:24 +0300

    Create Lab03.java

---
+++ Lab03.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Lab03 {
+    public static void main(String[] args) {
+        
+    }
+    
+    public static boolean isValid(int[] array) {
+        int  size, i , elements;
+        Scanner input = new Scanner (System.in);
+        System.out.println("Enter the size of the elements: ");
+        size = input.nextInt();
+        array = new int[size];
+        System.out.println("Enter the elments of the array: ");
+        elements = input.nextInt();
+        
+
+
+        
+        
+        
+        
+        return false;
+
+    }
+}
