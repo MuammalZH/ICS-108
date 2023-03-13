d67b415b99c97e70c30a7dafd88628a417078329
MuammalZH <120335187+MuammalZH@users.noreply.github.com>
2023-03-13 16:54:07 +0300

    mj

---
+++ Labs/Lab16/Lab16.java
@@ -0,0 +1,47 @@
+
+
+import java.io.File; 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+
+public class Lab16 {
+   
+ class SortStudents {
+    public static void main(String[] args) {
+        ArrayList<Integer> studentId = new ArrayList<>();
+
+        try {
+            File file = new File("unsortedStudents.txt");
+            Scanner scanner = new Scanner (file);
+            while (scanner.hasNextInt()) {
+                studentId.add(scanner.nextInt());
+            }
+            scanner.close();
+
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
+        }
+        
+        Collections.sort(studentId);
+        
+        try {
+            File sortedFile = new File("sortedStudents.txt");
+            PrintWriter writer = new PrintWriter(sortedFile);
+
+            for (int id : studentId) {
+                writer.println(id);
+            }
+            writer.close();
+
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
+        }
+        
+        System.out.println("Sorted students written to sortedStudents.txt");
+    }
+}
+}
