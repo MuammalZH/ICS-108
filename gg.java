501d15dbe54fb4dc8d48457af36b42c67d40c2c8
MuammalZH <120335187+MuammalZH@users.noreply.github.com>
2023-03-15 11:26:29 +0300

    Create gg.java

---
+++ gg.java
@@ -0,0 +1,11 @@
+public class gg {
+    public static void main(String[] args) {
+        
+        int i = 1, j = 2, k = 3;
+ if (i > j)
+ 	if (i > k)
+        System.out.println("A");
+   else
+        System.out.println("B");
+    }
+}
