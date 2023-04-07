a4b1334784c62ba639264b2f5259e153b1b66712
MuammalZH <120335187+MuammalZH@users.noreply.github.com>
2023-04-07 14:57:12 +0300

    Update Hw3.java
    y

--- Homework/Homework3/Hw3.java
+++ Homework/Homework3/Hw3.java
@@ -1,197 +1,196 @@
-import java.util.Arrays;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.ArrayList;
-public class Hw3 {
+// import java.util.Arrays;
+// import java.util.ArrayList;
+
+// public class Hw3 {
  
-    public static void main(String[] args) {
+//     public static void main(String[] args) {
      
-    int numberOFCards =4;
+//     int numberOFCards =4;
      
-    Card[] cards = new Card[numberOFCards];
+//     Card[] cards = new Card[numberOFCards];
      
-    cards[0] = new SilverCard(2001,0.15);
+//     cards[0] = new SilverCard(2001,0.15);
      
-    cards[0]
-    .purchase(500);
+//     cards[0]
+//     .purchase(500);
      
-    cards[0]
-    .purchase(100);
+//     cards[0]
+//     .purchase(100);
      
-    cards[1] = new GoldenCard(2002,50);
+//     cards[1] = new GoldenCard(2002,50);
      
-    cards[1]
-    .purchase(150);
+//     cards[1]
+//     .purchase(150);
      
-    cards[2] = new GoldenCard(2003,70);
+//     cards[2] = new GoldenCard(2003,70);
      
-    cards[2]
-    .purchase(100);
+//     cards[2]
+//     .purchase(100);
      
-    cards[2]
-    .purchase(300);
+//     cards[2]
+//     .purchase(300);
      
-    cards[3] = new SilverCard(2004,0.20);
+//     cards[3] = new SilverCard(2004,0.20);
      
-    cards[3]
-    .purchase(200);
+//     cards[3]
+//     .purchase(200);
      
-    cards[3]
-    .purchase(100);
+//     cards[3]
+//     .purchase(100);
      
-    System.out.println("All cards: ");
+//     System.out.println("All cards: ");
      
-    for(Card unsorted : cards)
-    {
+//     for(Card unsorted : cards)
+//     {
      
-    System.out.println(unsorted);}
+//     System.out.println(unsorted);}
      
-    System.out.println("---------------------------------");
+//     System.out.println("---------------------------------");
      
-    Arrays.sort(cards);
+//     Arrays.sort(cards);
      
-    System.out.println("Cards sorted by total amount+fee: ");
+//     System.out.println("Cards sorted by total amount+fee: ");
      
-    for(Card sorted : cards)
-    {
+//     for(Card sorted : cards)
+//     {
      
-    System.out.println(sorted);}
+//     System.out.println(sorted);}
      
-    System.out.println("---------------------------------");
+//     System.out.println("---------------------------------");
      
-    int sum = 0;
+//     int sum = 0;
      
-    for(Card goldenCards : cards)
-    {
+//     for(Card goldenCards : cards)
+//     {
      
-    if(goldenCards instanceof GoldenCard) sum += goldenCards.totalAmount();}
+//     if(goldenCards instanceof GoldenCard) sum += goldenCards.totalAmount();}
      
-    System.out.println("Golden cards total " + sum + " $");}}
+//     System.out.println("Golden cards total " + sum + " $");}}
      
-    abstract class Card implements Comparable<Card> {
+//     abstract class Card implements Comparable<Card> {
      
-    private int cardNumber;
+//     private int cardNumber;
      
-    private ArrayList<Double> transactions = new ArrayList<>();
+//     private ArrayList<Double> transactions = new ArrayList<>();
      
-    Card(int cardNumber)
-    {
+//     Card(int cardNumber)
+//     {
      
-    this.cardNumber = cardNumber;}
+//     this.cardNumber = cardNumber;}
      
-    public int getcardNumber() {
+//     public int getcardNumber() {
      
-    return cardNumber; }
+//     return cardNumber; }
      
-    public void setcardNumber(int cardNumber) {
+//     public void setcardNumber(int cardNumber) {
      
-    this.cardNumber = cardNumber;}
+//     this.cardNumber = cardNumber;}
      
-    public ArrayList<Double> getTransactions() {
+//     public ArrayList<Double> getTransactions() {
      
-    return transactions;}
+//     return transactions;}
      
-    public void setTransactions(ArrayList<Double> transactions) {
+//     public void setTransactions(ArrayList<Double> transactions) {
      
-    this.transactions = transactions;}
+//     this.transactions = transactions;}
      
-    @Override
+//     @Override
      
-    public String toString() {return String.format("Card no: %d %nTotal Amount: %.2f", cardNumber , totalAmount()
-    );}
+//     public String toString() {return String.format("Card no: %d %nTotal Amount: %.2f", cardNumber , totalAmount()
+//     );}
      
-    public abstract double totalAmount();
+//     public abstract double totalAmount();
      
-    public void purchase(double amount)
-    {this.transactions.add(amount);}
+//     public void purchase(double amount)
+//     {this.transactions.add(amount);}
      
-    @Override
+//     @Override
      
-    public boolean equals(Object that) {
+//     public boolean equals(Object that) {
      
-    if( that instanceof Card)
-    {
+//     if( that instanceof Card)
+//     {
      
-    Card obj = (Card) that;
+//     Card obj = (Card) that;
      
-    return (this.cardNumber == obj.cardNumber);}
+//     return (this.cardNumber == obj.cardNumber);}
      
-    else return false;}
+//     else return false;}
      
-    @Override
+//     @Override
      
-    public int compareTo(Card o) {
+//     public int compareTo(Card o) {
      
-    if(this.totalAmount() > o.totalAmount()
-    ) return 1;
+//     if(this.totalAmount() > o.totalAmount()
+//     ) return 1;
      
-    else if (this.totalAmount() == o.totalAmount()
-    ) return 0;
+//     else if (this.totalAmount() == o.totalAmount()
+//     ) return 0;
      
-    else return -1;}}
+//     else return -1;}}
      
-    class SilverCard extends Card {
+//     class SilverCard extends Card {
      
-    private double transactionFee;
+//     private double transactionFee;
      
      
-    SilverCard(int cardNumber, double transactionFee)
-    {
+//     SilverCard(int cardNumber, double transactionFee)
+//     {
      
-    super(cardNumber);
+//     super(cardNumber);
      
-    this.transactionFee = transactionFee;
+//     this.transactionFee = transactionFee;
      
-    }
+//     }
      
      
-    @Override
+//     @Override
      
-    public void purchase(double amount) {
+//     public void purchase(double amount) {
      
-    super.purchase(amount);
+//     super.purchase(amount);
      
-    super.getTransactions()
-    .add(amount * transactionFee);}
+//     super.getTransactions()
+//     .add(amount * transactionFee);}
      
-    @Override
+//     @Override
      
-    public double totalAmount() {
+//     public double totalAmount() {
      
-    double sum = 0;
+//     double sum = 0;
      
      
-    for (double i : super.getTransactions()
-    )
-    {
+//     for (double i : super.getTransactions()
+//     )
+//     {
      
-    sum += i;}
+//     sum += i;}
      
-    return sum;
+//     return sum;
      
-    }}
+//     }}
      
      
-    class GoldenCard extends Card{
+//     class GoldenCard extends Card{
      
-    private int fee;
+//     private int fee;
      
      
-    public GoldenCard(int cardNumber, int fee) {
+//     public GoldenCard(int cardNumber, int fee) {
      
-    super(cardNumber);
+//     super(cardNumber);
      
-    this.fee = fee;}
+//     this.fee = fee;}
      
      
-    @Override
+//     @Override
      
-    public double totalAmount() {
+//     public double totalAmount() {
      
-    double sum = 0.0;
+//     double sum = 0.0;
      
-    for (double i : super.getTransactions()
-    ) sum += i;
+//     for (double i : super.getTransactions()
+//     ) sum += i;
      
-    return sum + fee;}}
+//     return sum + fee;}}
     
