888a37add116e7d16e5fe46257eba527de9883ee
MuammalZH <120335187+MuammalZH@users.noreply.github.com>
2023-04-07 05:36:09 +0300

    h

--- Labs/Lab18/JavaFX.java
+++ Labs/Lab18/JavaFX.java
@@ -1,76 +1,78 @@
-import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.geometry.Pos;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
+// Lab 18 is solved in Intellij IDEA
 
-public class JavaFX extends Application {
-    Image[] images = {
-            new Image("https://dev.java/assets/images/java-logo-vert-blk.png"),
-            new Image("https://scand.com/wp-content/uploads/2021/04/Java.jpg"),
-            new Image("Image.png") };
-    ImageView imageView;
-    Button changeImageButton;
-    Button previousImageButton;
-    Button forwardImageButton;
-    int imageIndex = 0;
+// import javafx.application.Application;
+// import javafx.event.ActionEvent;
+// import javafx.event.EventHandler;
+// import javafx.geometry.Pos;
+// import javafx.scene.Scene;
+// import javafx.scene.control.Button;
+// import javafx.scene.image.Image;
+// import javafx.scene.image.ImageView;
+// import javafx.scene.layout.HBox;
+// import javafx.scene.layout.VBox;
+// import javafx.stage.Stage;
 
-    @Override
-    public void start(Stage stage) {
-        imageView = new ImageView(images[imageIndex]);
+// public class JavaFX extends Application {
+//     Image[] images = {
+//             new Image("https://dev.java/assets/images/java-logo-vert-blk.png"),
+//             new Image("https://scand.com/wp-content/uploads/2021/04/Java.jpg"),
+//             new Image("Image.png") };
+//     ImageView imageView;
+//     Button changeImageButton;
+//     Button previousImageButton;
+//     Button forwardImageButton;
+//     int imageIndex = 0;
 
-        ButtonHandler buttonHandler = new ButtonHandler();
-        changeImageButton = new Button("Change Image");
-        changeImageButton.setOnAction(buttonHandler);
-        previousImageButton = new Button("Previous Image");
-        previousImageButton.setOnAction(buttonHandler);
-        forwardImageButton = new Button("Forward Image");
-        forwardImageButton.setOnAction(buttonHandler);
+//     @Override
+//     public void start(Stage stage) {
+//         imageView = new ImageView(images[imageIndex]);
 
-        HBox buttonBox = new HBox(10, previousImageButton, changeImageButton, forwardImageButton);
-        buttonBox.setAlignment(Pos.CENTER);
+//         ButtonHandler buttonHandler = new ButtonHandler();
+//         changeImageButton = new Button("Change Image");
+//         changeImageButton.setOnAction(buttonHandler);
+//         previousImageButton = new Button("Previous Image");
+//         previousImageButton.setOnAction(buttonHandler);
+//         forwardImageButton = new Button("Forward Image");
+//         forwardImageButton.setOnAction(buttonHandler);
 
-        VBox vBox = new VBox(10, imageView, buttonBox);
-        vBox.setAlignment(Pos.CENTER);
+//         HBox buttonBox = new HBox(10, previousImageButton, changeImageButton, forwardImageButton);
+//         buttonBox.setAlignment(Pos.CENTER);
 
-        Scene scene = new Scene(vBox, 500, 530);
+//         VBox vBox = new VBox(10, imageView, buttonBox);
+//         vBox.setAlignment(Pos.CENTER);
 
-        stage.setTitle("Generate a Random Image");
-        stage.setScene(scene);
-        stage.show();
-    }
+//         Scene scene = new Scene(vBox, 500, 530);
 
-    void changeImage() {
-        imageView.setImage(images[imageIndex]);
-    }
+//         stage.setTitle("Generate a Random Image");
+//         stage.setScene(scene);
+//         stage.show();
+//     }
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+//     void changeImage() {
+//         imageView.setImage(images[imageIndex]);
+//     }
 
-    class ButtonHandler implements EventHandler<ActionEvent> {
-        @Override
-        public void handle(ActionEvent e) {
-            if (e.getSource() == changeImageButton) {
-                imageIndex = (int) (Math.random() * images.length);
-            } else if (e.getSource() == previousImageButton) {
-                imageIndex--;
-                if (imageIndex < 0) {
-                    imageIndex = images.length - 1;
-                }
-            } else if (e.getSource() == forwardImageButton) {
-                imageIndex++;
-                if (imageIndex >= images.length) {
-                    imageIndex = 0;
-                }
-            }
-            changeImage();
-        }
-    }
-}
+//     public static void main(String[] args) {
+//         launch(args);
+//     }
+
+//     class ButtonHandler implements EventHandler<ActionEvent> {
+//         @Override
+//         public void handle(ActionEvent e) {
+//             if (e.getSource() == changeImageButton) {
+//                 imageIndex = (int) (Math.random() * images.length);
+//             } else if (e.getSource() == previousImageButton) {
+//                 imageIndex--;
+//                 if (imageIndex < 0) {
+//                     imageIndex = images.length - 1;
+//                 }
+//             } else if (e.getSource() == forwardImageButton) {
+//                 imageIndex++;
+//                 if (imageIndex >= images.length) {
+//                     imageIndex = 0;
+//                 }
+//             }
+//             changeImage();
+//         }
+//     }
+// }
--- Labs/Lab19/JavaFX.java
+++ Labs/Lab19/JavaFX.java
@@ -1,108 +1,110 @@
-package Labs.Lab19;
+// Lab 19 is solved in Intellij IDEA
 
-package com.example.javafx;
+// package Labs.Lab19;
 
-import javafx.animation.Animation;
-import javafx.animation.KeyFrame;
-import javafx.animation.Timeline;
-import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.geometry.Pos;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import javafx.stage.Stage;
-import javafx.util.Duration;
+// package com.example.javafx;
 
-public class JavaFX extends Application {
+// import javafx.animation.Animation;
+// import javafx.animation.KeyFrame;
+// import javafx.animation.Timeline;
+// import javafx.application.Application;
+// import javafx.event.ActionEvent;
+// import javafx.event.EventHandler;
+// import javafx.geometry.Pos;
+// import javafx.scene.Scene;
+// import javafx.scene.control.Button;
+// import javafx.scene.image.Image;
+// import javafx.scene.image.ImageView;
+// import javafx.scene.layout.BorderPane;
+// import javafx.scene.layout.HBox;
+// import javafx.stage.Stage;
+// import javafx.util.Duration;
 
-    int currentIndex = 0;
-    boolean isPlaying = false;
-    Timeline timeline;
-    Button playPauseButton;
-    Button speedUpButton;
-    Button speedDownButton;
+// public class JavaFX extends Application {
 
-    @Override
-    public void start(Stage primaryStage) {
+//     int currentIndex = 0;
+//     boolean isPlaying = false;
+//     Timeline timeline;
+//     Button playPauseButton;
+//     Button speedUpButton;
+//     Button speedDownButton;
 
-        Image[] images = {
-                new Image("https://dev.java/assets/images/java-logo-vert-blk.png"),
-                new Image("https://scand.com/wp-content/uploads/2021/04/Java.jpg"),
-                new Image("Image.png") };
+//     @Override
+//     public void start(Stage primaryStage) {
 
-        ImageView imageView = new ImageView(images[0]);
+//         Image[] images = {
+//                 new Image("https://dev.java/assets/images/java-logo-vert-blk.png"),
+//                 new Image("https://scand.com/wp-content/uploads/2021/04/Java.jpg"),
+//                 new Image("Image.png") };
 
-        playPauseButton = new Button("Play");
-        playPauseButton.setOnAction(new ButtonHandler());
+//         ImageView imageView = new ImageView(images[0]);
 
-        speedUpButton = new Button("Speed+");
-        speedUpButton.setOnAction(new ButtonHandler());
+//         playPauseButton = new Button("Play");
+//         playPauseButton.setOnAction(new ButtonHandler());
 
-        speedDownButton = new Button("Speed-");
-        speedDownButton.setOnAction(new ButtonHandler());
+//         speedUpButton = new Button("Speed+");
+//         speedUpButton.setOnAction(new ButtonHandler());
 
-        HBox buttonBox = new HBox(10, playPauseButton, speedUpButton, speedDownButton);
-        buttonBox.setAlignment(Pos.CENTER);
+//         speedDownButton = new Button("Speed-");
+//         speedDownButton.setOnAction(new ButtonHandler());
 
-        BorderPane root = new BorderPane();
-        root.setCenter(imageView);
-        root.setBottom(buttonBox);
+//         HBox buttonBox = new HBox(10, playPauseButton, speedUpButton, speedDownButton);
+//         buttonBox.setAlignment(Pos.CENTER);
 
-        timeline = new Timeline(new KeyFrame(Duration.seconds(2), event -> {
-            currentIndex++;
-            if (currentIndex == images.length) {
-                currentIndex = 0;
-            }
-            imageView.setImage(images[currentIndex]);
-        }));
-        timeline.setCycleCount(Animation.INDEFINITE);
+//         BorderPane root = new BorderPane();
+//         root.setCenter(imageView);
+//         root.setBottom(buttonBox);
 
-        Scene scene = new Scene(root, 500, 600);
+//         timeline = new Timeline(new KeyFrame(Duration.seconds(2), event -> {
+//             currentIndex++;
+//             if (currentIndex == images.length) {
+//                 currentIndex = 0;
+//             }
+//             imageView.setImage(images[currentIndex]);
+//         }));
+//         timeline.setCycleCount(Animation.INDEFINITE);
 
-        primaryStage.setTitle("Lab-19");
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
+//         Scene scene = new Scene(root, 500, 600);
 
-    private class ButtonHandler implements EventHandler<ActionEvent> {
-        @Override
-        public void handle(ActionEvent event) {
-            if (event.getSource() == playPauseButton) {
-                if (isPlaying) {
-                    pauseAnimation();
-                } else {
-                    playAnimation();
-                }
-            } else if (event.getSource() == speedUpButton) {
-                if (isPlaying) {
-                    timeline.setRate(timeline.getRate() * 2);
-                }
-            } else if (event.getSource() == speedDownButton) {
-                if (isPlaying) {
-                    timeline.setRate(timeline.getRate() / 2);
-                }
-            }
-        }
-    }
+//         primaryStage.setTitle("Lab-19");
+//         primaryStage.setScene(scene);
+//         primaryStage.show();
+//     }
 
-    private void playAnimation() {
-        isPlaying = true;
-        playPauseButton.setText("Pause");
-        timeline.play();
-    }
+//     private class ButtonHandler implements EventHandler<ActionEvent> {
+//         @Override
+//         public void handle(ActionEvent event) {
+//             if (event.getSource() == playPauseButton) {
+//                 if (isPlaying) {
+//                     pauseAnimation();
+//                 } else {
+//                     playAnimation();
+//                 }
+//             } else if (event.getSource() == speedUpButton) {
+//                 if (isPlaying) {
+//                     timeline.setRate(timeline.getRate() * 2);
+//                 }
+//             } else if (event.getSource() == speedDownButton) {
+//                 if (isPlaying) {
+//                     timeline.setRate(timeline.getRate() / 2);
+//                 }
+//             }
+//         }
+//     }
 
-    private void pauseAnimation() {
-        isPlaying = false;
-        playPauseButton.setText("Play");
-        timeline.pause();
-    }
+//     private void playAnimation() {
+//         isPlaying = true;
+//         playPauseButton.setText("Pause");
+//         timeline.play();
+//     }
 
-    public static void main(String[] args) {
-        launch(args);
-    }
-}
+//     private void pauseAnimation() {
+//         isPlaying = false;
+//         playPauseButton.setText("Play");
+//         timeline.pause();
+//     }
+
+//     public static void main(String[] args) {
+//         launch(args);
+//     }
+// }
