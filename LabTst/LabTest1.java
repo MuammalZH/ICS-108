91ef1b27c46e79edb07b1db98c621f607601e676
MuammalZH <120335187+MuammalZH@users.noreply.github.com>
2023-05-12 16:46:40 +0300

    m

---
+++ LabTst/LabTest1.java
@@ -0,0 +1,43 @@
+package LabTst;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class LabTest1 {
+    public static void main(String[] args) {
+        String filePath = "MissingLuctures.txt";
+
+        try {
+            Map<String, String> missedLectures = new HashMap<>();
+
+            File file = new File(filePath);
+            Scanner scanner = new Scanner(file);
+
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] data = line.split(" ");
+                if (data.length >= 2) {
+                    String studentId = data[0];
+                    String missedLecture = data[1];
+
+                    if (missedLectures.containsKey(studentId)) {
+                        String previousMissedLectures = missedLectures.get(studentId);
+                        missedLectures.put(studentId, previousMissedLectures + ", " + missedLecture);
+                    } else {
+                        missedLectures.put(studentId, missedLecture);
+                    }
+                }
+            }
+
+            scanner.close();
+
+            for (Map.Entry<String, String> entry : missedLectures.entrySet()) {
+                String studentId = entry.getKey();
+                String missedLecture = entry.getValue();
+                System.out.println("Student " + studentId + " missed lectures: " + missedLecture);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
+        }}}
---
+++ Labs/Lab23/Lab24.java
@@ -0,0 +1,36 @@
+package Labs.Lab23;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
+
+public class Lab24 {
+    public static void main(String[] args) {
+        TreeMap<String, TreeSet<Integer>> majors = new TreeMap<>();
+
+        try (Scanner input = new Scanner(new File("ListOfStudents.txt"))) {
+            while (input.hasNext()) {
+                int id = input.nextInt();
+                String major = input.next();
+                TreeSet<Integer> ids = majors.computeIfAbsent(major, k -> new TreeSet<>());
+                ids.add(id);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
+        }
+
+        List<String> majorList = new ArrayList<>(majors.keySet());
+        List<TreeSet<Integer>> idList = new ArrayList<>(majors.values());
+
+        for (int i = 0; i < majorList.size(); i++) {
+            String major = majorList.get(i);
+            TreeSet<Integer> ids = idList.get(i);
+            System.out.println(major + ": " + ids);
+        }
+    }
+}
